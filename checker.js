a5eb1a114cdc9a043cbf8b7be4fd4469f04abb9b
c-austin <35517261+c-austin@users.noreply.github.com>
2021-06-28 12:53:48 -0400

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,7 +1,7 @@
 const userAgent = navigator.userAgent;
 const BASE_URL = 'https://ar-works.s3.ca-central-1.amazonaws.com/Test_Gazebo_01'
 const IOS_URL = `${BASE_URL}.usdz`
-const ANDROID_URL = `intent://arvr.google.com/scene-viewer/1.0?file=${BASE_URL}.glb?raw=true#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
+const ANDROID_URL = `intent://arvr.google.com/scene-viewer/1.0?file=${BASE_URL}.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
 const LANDING_URL = 'landing.html'
 
 
