f38be8fdcf7ee8a73eb036346837d33f9543db2c
c-austin <35517261+c-austin@users.noreply.github.com>
2021-06-28 12:37:12 -0400

    Add files via upload

--- checker.js
+++ checker.js
@@ -1,11 +1,13 @@
 const userAgent = navigator.userAgent;
-const RAW_ANDROID_URL = 'https://ar-works.s3.ca-central-1.amazonaws.com/Test_Gazebo_01.glb'
-const IOS_URL = 'https://ar-works.s3.ca-central-1.amazonaws.com/Test_Gazebo_01.usdz'
+const BASE_URL = 'https://ar-works.s3.ca-central-1.amazonaws.com/Test_Gazebo_01'
+const IOS_URL = `${BASE_URL}.usdz`
+const ANDROID_URL = `intent://arvr.google.com/scene-viewer/1.0?file=${BASE_URL}.glb?raw=true#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;`
 const LANDING_URL = 'landing.html'
+
 
 if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
     window.location.replace(IOS_URL);
-} else if (userAgent.includes('iPhone')) {
+} else if (userAgent.includes('Android')) {
     window.location.replace(ANDROID_URL);
 } else {
     window.location.replace(LANDING_URL);
