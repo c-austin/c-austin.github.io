ea660fab82ff461d135be96599d6b8313d05375f
c-austin <cory.austin@ryerson.ca>
2021-06-30 10:06:40 -0400

    hide until confirmed not iOS

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const APPLE_LINK = document.getElementById('apple-link');
+const MAIN = document.getElementById('main');
 const USER_AGENT = navigator.userAgent;
+
 if (USER_AGENT.includes('iPhone') || USER_AGENT.includes('iPad')) APPLE_LINK.click();
+else MAIN.classList.remove('hidden');
