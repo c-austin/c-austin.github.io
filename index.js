8b1b1c4e72dea062e70559b29e774e5dbbebb879
c-austin <cory.austin@ryerson.ca>
2021-06-30 09:27:34 -0400

    add forwarding for iOS

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const ANDROID_LINK = document.getElementById('android-link');
 const APPLE_LINK = document.getElementById('apple-link');
 const USER_AGENT = navigator.userAgent;
-
-if (USER_AGENT.includes('Android')) ANDROID_LINK.click();
 if (USER_AGENT.includes('iPhone') || USER_AGENT.includes('iPad')) APPLE_LINK.click();
